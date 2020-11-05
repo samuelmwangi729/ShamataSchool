daba056ad33d7ddfd67c49b38b60eef0cb64c67b
samuelmwangi <samuelmwangi729@gmail.com>
2020-11-05 18:01:07 +0300

    final one

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,3 @@ Route::get('/Vision',function(){
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
